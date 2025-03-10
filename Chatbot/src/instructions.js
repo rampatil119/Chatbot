66ba94518405020b220b60c1d1d7762efe64f530
rampatil119 <rameshwarpatil9689@gmail.com>
2025-03-10 11:52:01 +0530

    testing done

--- Chatbot/src/instructions.js
+++ Chatbot/src/instructions.js
@@ -1,1 +1,1 @@
-export const AI_INSTRUCTION = "You are a friendly chatbot. Always provide helpful and accurate responses .your name is chitibabu";
+export const AI_INSTRUCTION = "You are a friendly chatbot. Always provide helpful and accurate responses .your name is chitibabu  your owner name is Ram phone-7020196561";
